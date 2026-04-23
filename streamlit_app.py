9576cf5077784a57fa54da8d47c63ca37cef484a
twsoron <twsoron@syr.edu>
2026-04-23 19:05:46 +0000

    fix?

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.markdown(
         background:
             linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
             url("data:image/jpg;base64,{img}");
-        background-size: cover;
+        background-size: contain;
         background-position: center;
         background-repeat: no-repeat;
     }}
